3ec95909c46d499713b8b998c94fa65c7969a968
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2023-10-19 11:50:17 -0300

    Add files via upload

---
+++ JavaGrafico/AppGUI01.java
@@ -0,0 +1,13 @@
+package JavaGrafico;
+import javax.swing.JFrame;
+
+public class AppGUI01 {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame();
+		janela.setSize(300, 200);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+	}
+
+}
